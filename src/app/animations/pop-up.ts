8bacf90ecc756e6a094ccad9f9c70faa2d170d27
akmalkrmv <akmalkarimov94@gmail.com>
2022-09-07 23:28:03 +0500

    rename animation

--- src/app/animations/pop-up.ts
+++ src/app/animations/pop-up.ts
@@ -1,6 +1,6 @@
 import { trigger, transition, style, animate, keyframes } from '@angular/animations';
 
-export const popUp = trigger('pupUp', [
+export const popUp = trigger('popUp', [
   transition('* => active', [
     style({ position: 'absolute', zIndex: 999 }),
     animate('1s', keyframes([style({ top: '-100px' }), style({ top: '100px' }), style({ top: '-5000px' })])),
