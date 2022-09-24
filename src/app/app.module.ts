e3afe1327f2916be5de38444c82c7508c299c117
akmalkrmv <akmalkarimov94@gmail.com>
2022-09-25 01:36:24 +0500

    app module cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,45 +3,65 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 
+// Main module
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// TODO: Create Components module for these stuff:
 import { CardComponent } from './components/card/card.component';
-import { BattleComponent } from './battle/battle.component';
+import { CardListComponent } from './components/card-list/card-list.component';
 import { DiceComponent } from './components/dice/dice.component';
-import { PlayerDetailsComponent } from './player/player-details/player-details.component';
 import { DiceListComponent } from './components/dice-list/dice-list.component';
-import { EnemyComponent } from './player/enemy/enemy.component';
-import { VictoryComponent } from './battle/finish/victory/victory.component';
+import { HealthBarComponent } from './components/health-bar/health-bar.component';
+
+// TODO: Create Battle module for these stuff:
+import { BattleComponent } from './battle/battle.component';
 import { LossComponent } from './battle/finish/loss/loss.component';
+import { VictoryComponent } from './battle/finish/victory/victory.component';
+
+// TODO: Create Player module for these stuff:
+import { PlayerDetailsComponent } from './player/player-details/player-details.component';
+import { EnemyComponent } from './player/enemy/enemy.component';
 import { EffectsComponent } from './player/effects/effects.component';
+
+// TODO: Create Menu module for these stuff:
 import { ChooseCharacterComponent } from './menu/choose-character/choose-character.component';
 import { LevelComponent } from './menu/level/level.component';
 import { EquipmentComponent } from './menu/equipment/equipment.component';
 import { LevelStatsComponent } from './menu/level-stats/level-stats.component';
-import { CardListComponent } from './components/card-list/card-list.component';
-import { HealthBarComponent } from './components/health-bar/health-bar.component';
 import { BackpackComponent } from './menu/equipment/backpack/backpack.component';
 import { CardPreviewComponent } from './menu/equipment/card-preview/card-preview.component';
 import { EquippedComponent } from './menu/equipment/equipped/equipped.component';
 
 @NgModule({
   declarations: [
+    // Main App
     AppComponent,
+
+    // TODO: Components module stuff, remove declarations from here:
     CardComponent,
-    BattleComponent,
-    DiceComponent,
-    PlayerDetailsComponent,
-    DiceListComponent,
-    EnemyComponent,
-    VictoryComponent,
-    LossComponent,
-    EffectsComponent,
-    ChooseCharacterComponent,
-    LevelComponent,
-    EquipmentComponent,
-    LevelStatsComponent,
     CardListComponent,
+    DiceComponent,
+    DiceListComponent,
     HealthBarComponent,
+
+    // TODO: Battle module stuff, remove declarations from here:
+    BattleComponent,
+    LossComponent,
+    VictoryComponent,
+
+    // TODO: Player module stuff, remove declarations from here:
+    PlayerDetailsComponent,
+    EnemyComponent,
+    EffectsComponent,
+
+    // TODO: Menu module stuff, remove declarations from here:
+    ChooseCharacterComponent,
+    // Menu > Level stuff:
+    LevelComponent,
+    LevelStatsComponent,
+    // Menu > Equipment stuff:
+    EquipmentComponent,
     BackpackComponent,
     CardPreviewComponent,
     EquippedComponent,
