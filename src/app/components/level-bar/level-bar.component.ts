3dc36ae05ae9867289ca148bf725d5f8cc5f838a
Xojiakbar994 <94231309+Xojiakbar994@users.noreply.github.com>
2022-10-20 23:27:11 +0500

    inputs of level bar

--- src/app/components/level-bar/level-bar.component.ts
+++ src/app/components/level-bar/level-bar.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-level-bar',
   templateUrl: './level-bar.component.html',
-  styleUrls: ['./level-bar.component.scss']
+  styleUrls: ['./level-bar.component.scss'],
 })
 export class LevelBarComponent implements OnInit {
+  @Input() value!: number;
+  @Input() maxValue!: number;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
